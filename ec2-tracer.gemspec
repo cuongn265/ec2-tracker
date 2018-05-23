52849a844fb50719d1ba866801d4f8b813b26e93
cuongnm265 <cuongnm265@gmail.com>
2018-05-24 01:57:41 +0700

    Add thor

--- ec2-tracer.gemspec
+++ ec2-tracer.gemspec
@@ -13,9 +13,6 @@ Gem::Specification.new do |spec|
   spec.description   = %q{Write a longer description or delete this line.}
   spec.license       = "MIT"
 
-  spec.files         = `git ls-files -z`.split("\x0").reject do |f|
-    f.match(%r{^(test|spec|features)/})
-  end
   spec.bindir        = "exe"
   spec.executables   = spec.files.grep(%r{^exe/}) { |f| File.basename(f) }
   spec.require_paths = ["lib"]
@@ -23,4 +20,5 @@ Gem::Specification.new do |spec|
   spec.add_development_dependency "bundler", "~> 1.16"
   spec.add_development_dependency "rake", "~> 10.0"
   spec.add_dependency "aws-sdk-ec2"
+  spec.add_dependency "thor"
 end
--- lib/ec2/tracer.rb
+++ lib/ec2/tracer.rb
@@ -1,41 +1,32 @@
 require "ec2/tracer/version"
 require 'aws-sdk-ec2'
+require 'thor'
 
 module Ec2
   module Tracer
-    class Writer
+    class Writer < Thor
       WRITE_MODE = 'a'.freeze
       TEMP_IP = '0.0.0.0'
+      desc "write_config file_name host_prefix welligence_ml 2 10 ubuntu ~/.ssh/welligence.pem", "In ~/.ssh/config, mae add this line: `Include <where to run this command>/file_name`"
 
-      attr_reader :file_name, :host_prefix, :range, :user, :identity_file
-
-      def initialize(file_name = 'ec2.d', host_prefix = 'welligence_ml', range = (2..10), user = 'ubuntu', identity_file = '~/.ssh/welligence.pem' )
-        @file_name = file_name
-        @host_prefix = host_prefix
-        @range = range
-        @user = user
-        @identity_file = identity_file
-      end
-
-      def write_config
+      def write_config(file_name = 'ec2.d', host_prefix = 'welligence_ml', first = 2, last = 10, user = 'ubuntu', identity_file = '~/.ssh/welligence.pem' )
         ec2 = Aws::EC2::Resource.new(region: 'us-west-2')
         filepath = "#{Dir.pwd}/#{file_name}"
-
         FileUtils.rm_rf filepath
         FileUtils.touch filepath
 
-        ec2.instances({filters: [{name: 'tag:Name', values: range.map{|i| "#{host_prefix}_#{i}"} }]}).each do |i|
+        ec2.instances({filters: [{name: 'tag:Name', values: first.upto(last).map{|i| "#{host_prefix}_#{i}"} }]}).each do |i|
           instance_tag_name = i.tags.map(&:value).join()
           public_ip_address = i.public_ip_address
 
           puts "#{instance_tag_name} - #{public_ip_address}"
-          File.open(filepath, WRITE_MODE) { |f| f << ssh_host(host: instance_tag_name, host_name: public_ip_address) }
+          File.open(filepath, WRITE_MODE) { |f| f << ssh_host(host: instance_tag_name, host_name: public_ip_address, user: user, identity_file: identity_file) }
         end
       end
 
       private
 
-      def ssh_host(host:, host_name:)
+      def ssh_host(host:, host_name:, user:, identity_file:)
         %{
   # #{host}
   Host #{host}
